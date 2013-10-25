de3566e19ba7d38abfaaa412cf4b84abcdc3682f
Tim Garvin <tgarvin@protectus.com>
2013-10-25 11:01:57 -0400

    Let's see if cython works.

--- python/TrafMongo/setup.py
+++ python/TrafMongo/setup.py
@@ -1,10 +1,15 @@
-import os
+mport os
+import sys
+from zipfile import ZipFile # For egg manipulation
+import glob
+from fnmatch import fnmatch
 
 from setuptools import setup, find_packages
+from Cython.Distutils import build_ext
+from Cython.Build import cythonize
+
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 # "VERSION" is a keyword that the build system will look for.  Feel free to
 # change, but know that the build system is running sed, looking for
@@ -19,10 +24,11 @@ requires = [
     'pygeoip'
 ]
 
+cythonize_glob = 'trafmongo/*ommands.py'
+
 setup(name='TrafMongo',
       version=VERSION,
       description='TrafMongo',
-      long_description=README + '\n\n' +  CHANGES,
       classifiers=[
         "Programming Language :: Python",
         "Framework :: Pylons",
@@ -44,5 +50,38 @@ setup(name='TrafMongo',
       main = trafmongo:main
       """,
       paster_plugins=['pyramid'],
+      cmdclass = {'build_ext':build_ext},
+      ext_modules = cythonize(cythonize_glob)
       )
 
+# XXX: Apparently, this should actually be implimented as an extention of
+# setuptools.install, maybe?  http://stackoverflow.com/q/1321270
+if sys.argv[1] == 'bdist_egg':
+    dist_dir = "dist"
+    if '--dist-dir' in sys.argv:
+        dist_dir=sys.argv[sys.argv.index('--dist-dir')+1]
+
+    print "Stripping egg of proprietary source code... (hopefully)"
+    filenames = glob.glob(dist_dir+'/TrafMongo*.egg')
+    if len(filenames) != 1:
+        print "Not sure which egg file to use! Tell Tim to fix his setup.py."
+
+    # Move the original to make room for the new
+    target_name = filenames[0]
+    original = target_name + '.original'
+    os.rename(target_name,original)
+    egg = ZipFile(original)
+    newegg = ZipFile(target_name,'w')
+
+    # This is seperated from the above to avoid pseudo-race conditions.
+    for item in egg.infolist():
+        path = item.filename
+        if path.endswith('.c'):
+            print "\tRemoving " + path
+        else:
+            newegg.writestr(item, egg.read(path))
+
+    egg.close()
+    newegg.close()
+
+    print "Done removing proprietary source code"
