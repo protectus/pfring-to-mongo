8a3c9ea81e78d3af094050bd42057f6e0374b73c
Tim Garvin <tgarvin@protectus.com>
2013-11-06 21:28:28 -0500

    Renamed package sentry to protectus-sentry.

--- python/protectus-sentry/setup.py
+++ python/protectus-sentry/setup.py
@@ -6,7 +6,7 @@ import glob
 from setuptools import setup, find_packages
 
 # Cython actually accepts an extended glob pattern, where ** means more than one directory
-cythonize_glob = 'sentry/**/*.py'
+cythonize_glob = 'protectus_sentry/**/*.py'
 
 # "VERSION" is a keyword that the build system will look for.  Feel free to
 # change, but know that the build system is running sed, looking for
@@ -27,7 +27,7 @@ if sys.argv[1] in ['bdist_egg', 'install']:
         'ext_modules': cythonize(cythonize_glob)
     }
 
-setup(name='Sentry',
+setup(name='protectus-sentry',
       version=VERSION,
       description='Sentry shared code',
       classifiers=[
@@ -36,13 +36,13 @@ setup(name='Sentry',
       author='PROTECTUS',
       author_email='tgarvin@protectus.com',
       url='http://www.protectus.com',
-      keywords='sentry pymongo',
+      keywords='protectus',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
       install_requires=requires,
       tests_require=requires,
-      test_suite="sentry",
+      test_suite="protectus_sentry",
       **setup_settings
       )
 
@@ -54,7 +54,7 @@ if sys.argv[1] == 'bdist_egg':
         dist_dir=sys.argv[sys.argv.index('--dist-dir')+1]
 
     print "Stripping egg of proprietary source code... (hopefully)"
-    filenames = glob.glob(dist_dir+'/Sentry*.egg')
+    filenames = glob.glob(dist_dir+'/protectus-sentry*.egg')
     if len(filenames) != 1:
         print "Not sure which egg file to use! Tell Tim to fix his setup.py."
 
